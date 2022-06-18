3cd3e53a5431d05a1d91a4571afe124cc0b0fdb4
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-06-18 20:35:34 +0530

    :sparkles: Create Layouts.vue
    Added Layouts to App

---
+++ src/components/Layouts.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header class="z-50" />
+  <main class="container w-full mx-auto my-20">
+    <router-view />
+  </main>
+  <Footer />
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
