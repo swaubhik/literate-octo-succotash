f82603bbbeb745db1def254cb44e1e115f69edd0
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-06-18 20:51:11 +0530

    :wrench:  Update tailwind.config.js
    added fonts and colors to tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        display: ["Pacifico", "sans-serif"],
+        sans: ["DM Sans", ...defaultTheme.fontFamily.sans],
+        poppins: ["Poppins"],
+        quicksand: ["Quicksand"],
+        phil: ["Philosopher"],
       },
       colors: {
-        "regal-blue": "#243c5a",
+        primary: "#1B1B50",
       },
     },
   },
