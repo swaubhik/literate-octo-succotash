f88bc5908d2dd6afdcafe6e215da4378a575015f
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-06-18 21:02:12 +0530

    :rocket:  Create Button.vue
    Created button component for all button in the site with props

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="ml-3 inline-flex rounded-md shadow">
-    <a
-      target="_blank"
-      href="https://github.com/swaubhik/vite-vue3-router4-tailwind-starter"
-      class="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium leading-6 text-cyan-500 transition duration-150 ease-in-out hover:text-cyan-600 focus:outline-none"
-      >Learn more</a
-    >
-  </div>
+  <button class="flex flex-row justify-center items-center rounded-full cursor-pointer font-bold hover:filter-none">
+    {{ greetingMessage }}
+    <slot />
+  </button>
 </template>
 <script>
+import { ChevronRightIcon } from "@heroicons/vue/solid";
 export default {
   name: "Button",
+  components: {
+    ChevronRightIcon,
+  },
+  props: {
+    greetingMessage: String,
+  },
 };
 </script>
