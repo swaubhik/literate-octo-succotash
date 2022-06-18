277cc8a4094b1920bd184c57952ed179c9113055
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-06-18 20:51:21 +0530

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,11 @@
-<script>
-import Button from "../components/Button.vue";
-export default {
-  components: {
-    Button,
-  },
-};
-</script>
-
 <template>
-  <div>
-    <div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
-      <h2 class="text-3xl font-extrabold leading-9 tracking-tight text-slate-700 sm:text-4xl sm:leading-10">
-        Home Page
-        <br />
-        <br />
-        <span class="text-cyan-500">Vite + Vue 3 + Tailwind CSS</span>
-      </h2>
-      <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-        <div class="inline-flex rounded-md shadow">
-          <router-link
-            to="/about"
-            class="inline-flex items-center justify-center rounded-md border border-transparent bg-cyan-500 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-cyan-600 focus:outline-none"
-            >Next Page</router-link
-          >
-        </div>
-        <Button />
-      </div>
-    </div>
+  <div class="container mx-auto space-y-10 flex flex-col justify-center items-center py-40">
+    <h1 class="text-4xl font-sans font-bold">Home Page 🏠</h1>
+    <p class="tex-lg">Please go to the about page for more details</p>
+    <router-link
+      class="text-xl font-sans flex justify-center items-center max-w-xl text-gray-200 bg-slate-700 hover:bg-slate-800 px-4 py-2 rounded-lg shadow-lg"
+      to="/about"
+      >Go to About Page</router-link
+    >
   </div>
 </template>
